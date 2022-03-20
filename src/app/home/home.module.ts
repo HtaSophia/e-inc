fc5cdc085a02cd90daafe59e23519687e0047f15
Aghata Sophia <aghatasophia@gmail.com>
2022-03-20 17:12:29 -0300

    Create Welcome Component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
     declarations: [
-    HomeComponent
+    HomeComponent,
+    WelcomeComponent
   ],
     imports: [CommonModule],
 })
