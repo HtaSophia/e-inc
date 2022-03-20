dc6a86c626d696537c1c910a9057cd0cf3919b02
Aghata Sophia <aghatasophia@gmail.com>
2022-03-20 16:57:17 -0300

    Add Home Component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '**', component: NotFoundComponent }];
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
