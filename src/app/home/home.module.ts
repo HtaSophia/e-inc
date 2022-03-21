8957eda1230e7f94d25429b430cdc9309787167f
Aghata Sophia <aghatasophia@gmail.com>
2022-03-20 23:36:35 -0300

    Create About Component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-    declarations: [HomeComponent, WelcomeComponent],
+    declarations: [HomeComponent, WelcomeComponent, AboutComponent],
     imports: [CommonModule, RouterModule],
 })
 export class HomeModule {}
