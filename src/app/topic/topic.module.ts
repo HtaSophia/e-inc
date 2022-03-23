a835bab76bb43a4c6a33f62dc10b33f58b791df2
Aghata Sophia <aghatasophia@gmail.com>
2022-03-22 21:35:15 -0300

    Create Topic Component

--- src/app/topic/topic.module.ts
+++ src/app/topic/topic.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopicComponent } from './topic.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [TopicComponent],
     imports: [CommonModule],
 })
 export class TopicModule {}
