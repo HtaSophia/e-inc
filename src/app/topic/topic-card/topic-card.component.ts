96e8db5bceca9616d2e20d04b93834f1718c6cf9
Aghata Sophia <aghatasophia@gmail.com>
2022-04-03 16:21:24 -0300

    Update Topic Card Component

--- src/app/topic/topic-card/topic-card.component.ts
+++ src/app/topic/topic-card/topic-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Topic } from '../topic';
 
 @Component({
     selector: 'app-topic-card',
     templateUrl: './topic-card.component.html',
     styleUrls: ['./topic-card.component.scss'],
 })
-export class TopicCardComponent {}
+export class TopicCardComponent {
+    @Input()
+    public topic!: Topic;
+}
