812bceb77d0b6221568429fd5c29c7ce569203bd
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 15:45:20 -0300

    Update Article List

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Article } from '../article';
 
 @Component({
     selector: 'app-article-list',
     templateUrl: './article-list.component.html',
     styleUrls: ['./article-list.component.scss'],
 })
-export class ArticleListComponent {}
+export class ArticleListComponent {
+    @Input()
+    public articles: Article[] = [];
+
+    @Output()
+    public onCardClick = new EventEmitter<string>();
+
+    public onArticleCardClick(articleId: string): void {
+        this.onCardClick.emit(articleId);
+    }
+
+    public trackByArticle(_index: number, article: Article): string {
+        return article.id;
+    }
+}
