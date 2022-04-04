a276d282c9eab1bb5e195a4eae9993d9644d838f
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:33:21 -0300

    Create Article Card Component

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleComponent } from './article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleCardComponent } from './article-card/article-card.component';
 
 @NgModule({
-    declarations: [ArticleComponent, ArticleListComponent],
+    declarations: [ArticleComponent, ArticleListComponent, ArticleCardComponent],
     imports: [CommonModule, ArticleRoutingModule],
 })
 export class ArticleModule {}
