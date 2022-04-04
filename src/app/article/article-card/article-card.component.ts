9c55a07c9558fdd3d8e4324b8d361ffe515de40f
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 15:45:12 -0300

    Update Article Card

--- src/app/article/article-card/article-card.component.ts
+++ src/app/article/article-card/article-card.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ColorService } from '../../shared/services/color.service';
+import { Article } from '../article';
 
 @Component({
     selector: 'app-article-card',
     templateUrl: './article-card.component.html',
     styleUrls: ['./article-card.component.scss'],
 })
-export class ArticleCardComponent {}
+export class ArticleCardComponent implements OnInit {
+    @Input()
+    public article!: Article;
+
+    @Input()
+    public disabled = false;
+
+    @Output()
+    public onArticleCardClick = new EventEmitter<string>();
+
+    public backgroundColor = '';
+
+    constructor(private readonly colorService: ColorService) {}
+
+    public ngOnInit(): void {
+        this.backgroundColor = this.colorService.getColor();
+    }
+
+    public onCardClick(): void {
+        this.onArticleCardClick.emit(this.article.id);
+    }
+}
