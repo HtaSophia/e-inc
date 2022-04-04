fc231580cb57c8cea22b1aa0ea275cf1e5290320
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:04:04 -0300

    Add getAll topics method to the topic service

--- src/app/topic/topic.service.ts
+++ src/app/topic/topic.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
+import { TOPICS } from './constants/topics.constant';
+import { Topic } from './topic';
 
 @Injectable({
     providedIn: 'root',
 })
-export class TopicService {}
+export class TopicService {
+    private topics: Topic[] = TOPICS;
+
+    public getAll(): Topic[] {
+        return this.topics;
+    }
+}
