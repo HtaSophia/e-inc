123e353d1d6c5fb543492e049510cf5b7e5f8225
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:31:18 -0300

    Create Article List Component

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article.component';
 
 const routes: Routes = [
     {
         path: '',
+        component: ArticleComponent,
     },
 ];
 
--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleComponent } from './article.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
-    declarations: [ArticleComponent],
+    declarations: [ArticleComponent, ArticleListComponent],
     imports: [CommonModule, ArticleRoutingModule],
 })
 export class ArticleModule {}
