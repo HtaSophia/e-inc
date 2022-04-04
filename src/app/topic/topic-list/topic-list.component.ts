cda1dff9a3112f69b7e3144e73035b6e030947ea
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:03:41 -0300

    Add click event to the topic list

--- src/app/topic/topic-list/topic-list.component.ts
+++ src/app/topic/topic-list/topic-list.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Topic } from '../topic';
 
 @Component({
     selector: 'app-topic-list',
     templateUrl: './topic-list.component.html',
     styleUrls: ['./topic-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TopicListComponent {}
+export class TopicListComponent {
+    @Input()
+    public topics: Topic[] = [];
+
+    @Output()
+    public onCardClick = new EventEmitter<string>();
+
+    public onTopicCardClick(topicId: string): void {
+        this.onCardClick.emit(topicId);
+    }
+
+    public trackByTopic(_index: number, topic: Topic): string {
+        return topic.id;
+    }
+}
