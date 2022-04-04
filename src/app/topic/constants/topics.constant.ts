89e49975920d058015fca9e17d99be1f5dbed409
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:02:40 -0300

    Create a topic list constant

---
+++ src/app/topic/constants/topics.constant.ts
@@ -0,0 +1,10 @@
+import { Topic } from '../topic';
+
+export const TOPICS: Topic[] = [
+    { id: '0', image: 'all-articles.jpg', title: 'Todos Artigos' },
+    { id: '1', image: 'information.jpg', title: 'Informações Gerais' },
+    { id: '2', image: 'social-interaction.jpg', title: 'Interação Social' },
+    { id: '3', image: 'teaching.jpeg', title: 'Sala de Aula' },
+    { id: '4', image: 'conversation.jpeg', title: 'Suporte' },
+    { id: '5', image: 'study.jpg', title: 'Materiais para Estudo' },
+];
--- src/app/topic/topic.ts
+++ src/app/topic/topic.ts
@@ -2,5 +2,4 @@ export interface Topic {
     id: string;
     title: string;
     image: string;
-    articles?: string[];
 }
