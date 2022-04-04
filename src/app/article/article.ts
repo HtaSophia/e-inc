e012962b5ac8af663662a1e8dba330096fd3405a
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 15:45:00 -0300

    Update Article Model

--- src/app/article/article.ts
+++ src/app/article/article.ts
@@ -1,6 +1,15 @@
+import { Params } from '@angular/router';
+
 export interface Article {
     id: string;
     title: string;
     htmlContent: string;
     topicId: string;
+}
+
+export type ArticleParams = Params & { topicId?: string };
+
+export interface ArticleImageAndTitle {
+    image: string;
+    title: string;
 }
