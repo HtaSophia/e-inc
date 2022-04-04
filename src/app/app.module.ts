9ab604e0c6f943aad4fa0b6a2ef45169a0d63558
Aghata Sophia <aghatasophia@gmail.com>
2022-04-04 12:01:52 -0300

    Add Topics route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -8,11 +10,14 @@ import { NavbarComponent } from './core/navbar/navbar.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import { TopicModule } from './topic/topic.module';
 
 @NgModule({
     declarations: [AppComponent, NavbarComponent, NotFoundComponent],
     imports: [
+        CommonModule,
         BrowserModule,
+        RouterModule,
         AppRoutingModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: environment.production,
@@ -22,6 +27,7 @@ import { HomeModule } from './home/home.module';
         }),
         SharedModule,
         HomeModule,
+        TopicModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
